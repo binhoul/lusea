ad29c42ef05eb961e2fd8a1caeb284e340891a8e
binhoul <saintli86@163.com>
2014-09-03 23:18:03 +0800

    Update home.php

--- includes/views/home.php
+++ includes/views/home.php
@@ -2,13 +2,13 @@
 
 <div data-role="page">
   <div data-role="content">
-	<form action='index.php'>
-	  <div data-role="fieldcontain">
-	    <label for="cn_name">卡牌名称：</label>
-	    <input type="text" name="cn_name" id="cn_name">
-	    <input type="submit" data-inline="true" value="查询">
-	  </div>
-	</form>
+    <form action='index.php'>
+      <div data-role="fieldcontain">
+        <label for="cn_name">卡牌名称：</label>
+        <input type="text" name="cn_name" id="cn_name">
+        <input type="submit" data-inline="true" value="查询">
+      </div>
+    </form>
   </div>
 </div>
 <?php render('_footer')?>
